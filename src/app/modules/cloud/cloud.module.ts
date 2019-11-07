c2d08cb2a9543c64f617049940d325f017637f89
Administrator <Administrator@USER-20191029RB>
2019-11-07 08:16:47 +0800

    ng-zero的添加和主頁的修改

---
+++ src/app/modules/cloud/cloud.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MycloudComponent } from './mycloud/mycloud.component';
+
+
+
+@NgModule({
+  declarations: [MycloudComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CloudModule { }
---
+++ src/app/modules/cloud/mycloud/mycloud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mycloud',
+  templateUrl: './mycloud.component.html',
+  styleUrls: ['./mycloud.component.css']
+})
+export class MycloudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
